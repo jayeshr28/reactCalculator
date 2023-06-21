e0aad1a8cd54b606674087d6bb5f6e0d95f54516
jayeshr28 <jayeshrathore2810@gmail.com>
2023-06-21 17:39:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+import Calculator from './components/Task2';
+
+
+function App() {
+  const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff'];
+  return (
+    <>
+    ]
+  <div>
+    <Calculator></Calculator>
+  </div>
+    </>
+    
+  );
+}
+
+export default App;
+
